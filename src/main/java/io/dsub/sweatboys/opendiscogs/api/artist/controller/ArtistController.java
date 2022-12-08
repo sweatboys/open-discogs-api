9f361d33422f6ddff676b98e49640c091cd7b37e
theVelopr <immortaldk@gmail.com>
2022-12-08 16:50:14 +0900

    fix: pluralized resource endpoint.

--- src/main/java/io/dsub/sweatboys/opendiscogs/api/artist/controller/ArtistController.java
+++ src/main/java/io/dsub/sweatboys/opendiscogs/api/artist/controller/ArtistController.java
@@ -7,13 +7,11 @@ import io.dsub.sweatboys.opendiscogs.api.core.response.ResponseDTO;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 @Validated
@@ -21,16 +19,16 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class ArtistController {
   private final ArtistService service;
-  @GetMapping("artist")
+  @GetMapping("artists")
   public Mono<ResponseDTO<Artist>> searchArtists(ArtistQuery query, Pageable pageable) {
     return service.findArtists(query, pageable).
-        flatMap(dto -> Mono.just(dto.withResourceURI("/artist")));
+        flatMap(dto -> Mono.just(dto.withResourceURI("/artists")));
   }
 
-  @GetMapping("artist/{id}")
+  @GetMapping("artists/{id}")
   public Mono<ResponseDTO<Artist>> getArtist(@PathVariable("id") @Valid @NotNull @Min(1) long id) {
     return service.findArtist(id)
-        .flatMap(dto -> Mono.just(dto.withResourceURI("/artist/" + id)
+        .flatMap(dto -> Mono.just(dto.withResourceURI("/artists/" + id)
         .withPageNumber(dto.getPageNumber() + 1)));
   }
 }
