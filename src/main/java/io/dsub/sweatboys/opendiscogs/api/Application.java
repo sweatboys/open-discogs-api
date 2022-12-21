f8e61f4eb715d601412bfaed0b14874853bd1c00
state303 <state303@dsub.io>
2022-12-21 12:31:12 +0900

    feat: routes actuator to API_SERVER_HOST target

--- src/main/java/io/dsub/sweatboys/opendiscogs/api/Application.java
+++ src/main/java/io/dsub/sweatboys/opendiscogs/api/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 // {x-release-please-start-version}
-@OpenAPIDefinition(info = @Info(title = "Open Discogs API", version = "1.1.0", description = "Documentation APIs v0.0.0"))
+@OpenAPIDefinition(info = @Info(title = "Open Discogs API", version = "1.1.0", description = "Documentation APIs 1.1.0"))
 // {x-release-please-end}
 public class Application {
 
