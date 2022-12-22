b6d8906fd9274973d8d2b7db99a8bf15e8655092
state303 <state303@dsub.io>
2022-12-22 23:55:59 +0900

    fix: adds default meta info for WebFluxHandler

--- src/main/java/io/dsub/sweatboys/opendiscogs/api/config/PageableWebFluxConfiguration.java
+++ src/main/java/io/dsub/sweatboys/opendiscogs/api/config/PageableWebFluxConfiguration.java
@@ -2,6 +2,7 @@ package io.dsub.sweatboys.opendiscogs.api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.ReactivePageableHandlerMethodArgumentResolver;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurationSupport;
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
 
@@ -14,5 +15,19 @@ public class PageableWebFluxConfiguration extends WebFluxConfigurationSupport {
     resolver.setOneIndexedParameters(true);
     resolver.setMaxPageSize(30);
     configurer.addCustomResolver(resolver);
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/webjars/**") // setup webjar pattern to match our springdoc webjar.
+        .addResourceLocations("classpath:META-INF/resources/webjars/") // this is the path we are interested in
+        .resourceChain(false);
+    registry.addResourceHandler("/**") // Matcher for default, static resources (default configs)
+        .addResourceLocations(
+            "classpath:META-INF/resources/",
+            "classpath:resources/",
+            "classpath:static/",
+            "classpath:public/")
+        .resourceChain(false);
   }
 }
